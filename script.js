4024be1c6e08a5545f3d71a5fd881cdb63adc030
eracknet <144846516+swayam8576@users.noreply.github.com>
2023-12-30 18:30:28 +0530

    initiate

---
+++ script.js
@@ -0,0 +1,9 @@
+// You can add JavaScript animations or dynamic effects here
+// Example: Scroll animations using AOS library
+// More info: https://michalsnik.github.io/aos/
+AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+  });
+  
